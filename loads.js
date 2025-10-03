43011f1784bb0ae73c48ca851c005f4b8fdd37ac
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-10-02 18:42:11 -0600

    55

--- loads.js
+++ loads.js
@@ -52,7 +52,8 @@ const guests = [
     { id: 51, name: "Odilia Tunchez", passes: 1 },
     { id: 52, name: "Víctor y Aura", passes: 2 },
     { id: 53, name: "Sandra Tunchez", passes: 1 },
-    { id: 54, name: "Fabiola Marroquín", passes: 1 }
+    { id: 54, name: "Fabiola Marroquín", passes: 1 },
+    { id: 55, name: "Alicia Rivera e hijas", passes: 3 }
 ];
   
 
